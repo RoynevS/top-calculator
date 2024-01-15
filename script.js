090b5ea55fd47ac4887875585742501380352318
RoynevS <109765130+RoynevS@users.noreply.github.com>
2024-01-15 14:54:54 +0100

    Add function to operate on the numbers

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ const divide = (a, b) => a / b;
 let num1;
 let num2;
 let operator;
+
+const operate = (num1, operator, num2) => {
+  switch (operator) {
+    case "+":
+      add(num1, num2);
+      break;
+    case "-":
+      subtract(num1, num2);
+      break;
+    case "*":
+      multiply(num1, num2);
+      break;
+    case "/":
+      divide(num1, num2);
+      break;
+  }
+}
