77cb4068fadb005a4325e836e5849eb152008d8e
RoynevS <109765130+RoynevS@users.noreply.github.com>
2024-01-15 15:38:19 +0100

    Make calculator work for num operator num
    buttons work and populate the display, when equal is pressed it will
    calculate and update the display with the solution. Right now it only
    works for calculations that include exactly 2 numbers separated by one
    operator. The chaining of operations without pressing equal does not
    work.

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b; 
+const display = document.querySelector(".display");
+const numbers = document.querySelectorAll(".number--btn");
+const operators = document.querySelectorAll(".operator--btn");
+const equal = document.querySelector("#equal");
+
+const add = (a, b) => Number(a) + Number(b);
+const subtract = (a, b) => Number(a) - Number(b);
+const multiply = (a, b) => Number(a) * Number(b);
+const divide = (a, b) => Number(a) / Number(b); 
 
 let num1;
 let num2;
 let operator;
+let displayValue;
 
 const operate = (num1, operator, num2) => {
   switch (operator) {
     case "+":
-      add(num1, num2);
-      break;
+      return add(num1, num2);
     case "-":
-      subtract(num1, num2);
-      break;
+      return subtract(num1, num2);
     case "*":
-      multiply(num1, num2);
-      break;
+      return multiply(num1, num2);
     case "/":
-      divide(num1, num2);
-      break;
+      return divide(num1, num2);
   }
 }
+
+
+numbers.forEach(number => number.addEventListener("click", (event) => {
+  display.textContent += `${event.target.textContent}`;
+  displayValue = display.textContent;
+}));
+
+operators.forEach(operator => operator.addEventListener("click", (event) => {
+  display.textContent += ` ${event.target.textContent} `;
+}));
+
+equal.addEventListener("click", () => {
+  [num1, operator, num2] = displayValue.split(" ");
+  display.textContent = operate(num1, operator, num2);
+});
